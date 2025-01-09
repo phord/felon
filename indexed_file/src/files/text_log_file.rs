8aeef38c20e178a7dca704343f78b3dc75596972
Phil Hord <phil.hord@gmail.com>
2025-01-09 13:23:21 -0800

    Avoid unnecessary seeks
    Seeking to the start of each line is my performance killer.  I can avoid
    actually seeking by tracking the current position and skipping the
    seek when we're already in the right place.
    
    The big issue is that bufread always resets the position without
    considering whether it's already the current position.  So it always
    resets the buffer, and this negates the whole benefit of bufread.
    
    Note this trick is already implemented in CompressedFiles from
    long ago.

--- indexed_file/src/files/text_log_file.rs
+++ indexed_file/src/files/text_log_file.rs
@@ -8,12 +8,13 @@ use crate::files::Stream;
 pub struct TextLogFile {
     file: BufReader<File>,
     len: usize,
+    pos: u64,
 }
 
 impl TextLogFile {
     pub fn new(file: BufReader<File>) -> std::io::Result<TextLogFile> {
         let len = file.get_ref().metadata()?.len() as usize;
-        Ok(TextLogFile { file, len })
+        Ok(TextLogFile { file, len, pos: 0})
     }
 }
 
@@ -35,18 +36,27 @@ impl BufRead for TextLogFile {
     }
 
     fn consume(&mut self, amt: usize) {
+        self.pos += amt as u64;
         self.file.consume(amt);
     }
 }
 
 impl Read for TextLogFile {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        self.file.read(buf)
+        let bytes = self.file.read(buf)?;
+        // self.pos += bytes as u64;
+        Ok(bytes)
     }
 }
 
 impl Seek for TextLogFile {
     fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+        if let std::io::SeekFrom::Start(n) = pos {
+            if self.pos == n {
+                return Ok(n)
+            }
+            self.pos = n;
+        }
         self.file.seek(pos)
     }
 }
