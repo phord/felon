02c3a5f0d2e88e95f60874666b0c8ed49574fd30
Phil Hord <phil.hord@gmail.com>
2025-02-13 21:27:51 -0800

    Let Esc cancel search prompts

--- grok/src/input_line.rs
+++ grok/src/input_line.rs
@@ -1,11 +1,25 @@
 use reedline::{DefaultPrompt, DefaultPromptSegment, Reedline, Signal};
+use {
+    reedline::{KeyCode, KeyModifiers},
+    reedline::{default_emacs_keybindings, Emacs, ReedlineEvent},
+  };
 
 #[derive(Default)]
 pub struct InputLine { }
 
+
 impl InputLine {
     pub fn run(&mut self, prompt: &str) -> Option<String> {
-        let mut line_editor = Reedline::create();
+
+        let mut keybindings = default_emacs_keybindings();
+        keybindings.add_binding(
+            KeyModifiers::NONE,
+            KeyCode::Esc,
+            ReedlineEvent::CtrlC,
+        );
+        let edit_mode = Box::new(Emacs::new(keybindings));
+
+        let mut line_editor = Reedline::create().with_edit_mode(edit_mode);
         let prompt = DefaultPrompt {
                 left_prompt: DefaultPromptSegment::Basic(prompt.to_string()),
                 .. DefaultPrompt::default()
