d2709af304c763bc66e016fd969ef5c01fc11f3e
Phil Hord <phil.hord@gmail.com>
2025-02-27 18:01:16 -0800

    Refactor arg parser into generalized config message system
    Basic implementation parses args into meaning config settings, dispatches
    settings individually to to the config, and collects them in the struct.  Some
    future change will connect the configs together so different modules can
    process incoming settings as needed.

--- grok/src/config.rs
+++ grok/src/config.rs
@@ -1,5 +1,22 @@
 use std::path::PathBuf;
-use pico_args::Arguments;
+use itertools::Itertools;
+
+pub enum ConfigItem {
+    OpenFile(PathBuf),
+    Chop(bool),
+    AltScreen(bool),
+    Color(bool),
+    Visual(bool),
+    MouseScroll(u16),
+    // HideBefore(DateTime),
+    // HideAfter(DateTime),
+    // Search(String),
+    // FilterIn(String),
+    // FilterOut(String),
+    // Style(String, PattColor),
+    // Match(String, PattColor),
+
+}
 
 #[derive(Debug, Clone)]
 pub struct Config {
@@ -12,6 +29,13 @@ pub struct Config {
     pub mouse_scroll: u16,      // Number of lines to scroll with mouse-wheel
 }
 
+#[derive(Debug)]
+pub enum Error {
+    FileNotFound(String),
+    ExpectedInt(String),
+    ExpectedArgumentFor(String),
+    UnknownArgument(String),
+}
 
 const HELP: &str = "\
 App
@@ -26,7 +50,6 @@ OPTIONS:
   -S --chop-long-lines  Chop long lines instead of wrapping
   -X                    Skip terminal config/cleanup such as using the alternate screen
   -C --color            Use color highlighting of parsed lines
-  -c --semantic-color   Color numbers with a hashed color wheel
   -V --version          Display version information
 
 ARGS:
@@ -46,48 +69,86 @@ impl Config {
         }
     }
 
-// fn main() {
-//     let args = match parse_args() {
-//         Ok(v) => v,
-//         Err(e) => {
-//             eprintln!("Error: {}.", e);
-//             std::process::exit(1);
-//         }
-//     };
-
-//     println!("{:#?}", args);
-// }
-
-    pub fn from_env() -> Result<Config, pico_args::Error> {
-        let mut pargs = pico_args::Arguments::from_env();
-
-        if pargs.contains(["-h", "--help"]) {
-            print!("{}", HELP);
-            std::process::exit(0);
-        }
-
+    /// Parse commandline arguments into config options
+    pub fn from_env() -> Result<Config, Error> {
         let mut cfg = Config::new();
-        cfg.parse_args(pargs);
+        cfg.parse_args()?;
         Ok(cfg)
     }
 
-    // TODO: Need some way to handle "toggle" values; eg., -S at runtime toggles slice
-    fn parse_args(&mut self, mut pargs: Arguments) {
-        if pargs.contains(["-S", "--chop-long-lines"]) { self.chop = true; }
-        if pargs.contains(["-X", "--no-alternate-screen"]) { self.altscreen = false; }
-        if pargs.contains(["-C", "--color"]) { self.color = true; }
-        if pargs.contains(["-V", "--version"]) { self.version = true; }
-        if pargs.contains(["-M", "--mouse"]) { self.mouse = true; }
+    /// Handler for each config option we know. Accumulates settings into self
+    pub fn receive_item(&mut self, item: ConfigItem) {
+        match item {
+            ConfigItem::OpenFile(path) => self.filename.push(path),
+            ConfigItem::Chop(chop) => self.chop = chop,
+            ConfigItem::AltScreen(altscreen) => self.altscreen = altscreen,
+            ConfigItem::Color(color) => self.color = color,
+            ConfigItem::Visual(visual) => self.mouse = visual,
+            ConfigItem::MouseScroll(scroll) => self.mouse_scroll = scroll,
+        }
+    }
 
-        // Parse remaining args as input filenames
-        for ostr in pargs.finish() {
-            if let Some(s) = ostr.to_str() {
-                if s.as_bytes().first() == Some(&b'-') {
-                    eprintln!("Error: Unknown argument: {:?}", ostr);
-                    std::process::exit(1);
+    /// Parse a string argument and optionally, the word after it, into a ConfigItem.
+    /// Unrecognized switches are interpreted as files to add to the queue.
+    pub fn parse_item(&mut self, item: &str, arg: Option<&str>) -> Result<(ConfigItem, bool) , Error> {
+        let (arg, used) = if let Some(args) = item.split_once("=") {
+            (Some(args.1), false)
+        } else {
+            (arg, true)
+        };
+
+        let mut consumed = false;
+        let cfg = match item {
+            "-S" | "--chop-long-lines" => ConfigItem::Chop(!self.chop),
+            "-X" | "--no-alternate-screen" => ConfigItem::AltScreen(!self.altscreen),
+            "-C" | "--color" => ConfigItem::Color(!self.color),
+            "-M" | "--mouse" => ConfigItem::Visual(!self.mouse),
+            "-H" | "--help" => todo!("help"),
+            "-V" | "--version" => todo!("version"),
+            "-W" | "--wheel-lines" => {
+                if let Some(arg) = arg {
+                    if let Ok(num) = arg.parse::<u16>() {
+                        consumed = used;
+                        ConfigItem::MouseScroll(num)
+                    } else {
+                        return Err(Error::ExpectedInt(arg.to_string()));
+                    }
+                } else {
+                    return Err(Error::ExpectedArgumentFor(item.to_string()));
+                }
+            },
+            _ => ConfigItem::OpenFile(PathBuf::from(item)),
+        };
+        Ok((cfg, consumed))
+    }
+
+
+
+    // pub fn to_config_item();
+    // TODO: Need some way to handle "toggle" values; eg., -S at runtime toggles slice
+    fn parse_args(&mut self) -> Result<(), Error>{
+        let mut skip = false;
+        for arg_pairs in std::env::args()
+                .chain(std::iter::once("".to_string()))
+                .skip(1)
+                .tuple_windows() {
+            if skip {
+                skip = false;
+                continue;
+            }
+            let (item, arg) = arg_pairs;
+            let cfg = self.parse_item(&item, Some(&arg));
+            match cfg {
+                Ok((item, consumed)) => {
+                    self.receive_item(item);
+                    skip = consumed;
+                },
+                Err(e) => {
+                    eprintln!("Error: Parsing {:?}: {:?}", item, e);
+                    return Err(e)
                 }
             }
-            self.filename.push(PathBuf::from(ostr));
         }
+        Ok(())
     }
 }
